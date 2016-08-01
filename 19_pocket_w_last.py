7506aae7b750026dee1b4336a11229d3be75bd8f
YuChia <erwin.yuchia.chen@gmail.com>
2016-08-01 17:01:38 +0800

    add title to all histogram

--- 19_pocket_w_last.py
+++ 19_pocket_w_last.py
@@ -7,7 +7,7 @@ LOOP_TIMES = 2000
 def main():
     error_rate_list, average_error_rate = util.run_pocket(TRAIN_PATH, VERIFY_PATH, UPDATE_TIMES, LOOP_TIMES, use_w_last=True)
     print average_error_rate
-    util.create_histogram(error_rate_list)
+    util.create_histogram(error_rate_list, hist_title=r'$\mathbf{w}_{50}\  \mathrm{\ after\ updating\ 50\ times}$', x_label='# of Errors')
 
 
 if __name__ == '__main__':
