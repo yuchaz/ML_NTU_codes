40ce96183c929da4d3517bd4b50656ce53b8ed4e
YuChia <erwin.yuchia.chen@gmail.com>
2016-10-07 14:46:09 -0700

    Clarify the q19

--- hw2/question19.py
+++ hw2/question19.py
@@ -3,8 +3,9 @@ from hw2.utilities import train_decision_stump, get_config
 
 def main():
     train_path, = get_config('train_path')
-    best_of_best = train_decision_stump(train_path)
-    print best_of_best
+    Ein_min, min_s, min_theta, min_dimension = train_decision_stump(train_path)
+    print 'the best of best Ein is {0}, with s={1}, theta={2}, i={3}'.format(
+        Ein_min, min_s, min_theta, min_dimension)
 
 if __name__ == '__main__':
     main()
