41684b3491cf52ba5826abe77a49a0cf9c6ed88b
YuChia <erwin.yuchia.chen@gmail.com>
2016-10-07 12:54:03 -0700

    Put find Ein min to hw2.util

--- hw2/question17.py
+++ hw2/question17.py
@@ -1,27 +1,13 @@
 import numpy as np
 from pkg import utilities as util
-from hw2.utilities import generate_data, get_config
+from hw2.utilities import generate_data, get_config, find_min_Ein
 
 def main():
     Ein_list = []
     loop_time, data_size, flipping_prob = get_config()
     for rgn in range(loop_time):
         x, y, split_point = generate_data(data_size, flipping_prob)
-        theta_position_list = [i-split_point for i in x]
-        Ein_min = 1.1
-        min_theta = -1.1
-        min_s = 0
-        err = 0
-        for theta in theta_position_list:
-            for s in range (-1,3,2):
-                for n in range(len(x)):
-                    if s*util.sign(x[n]-theta) != y[n]:
-                        err += 1
-                Ein = float(err) / (data_size*2)
-                if Ein <= Ein_min:
-                    Ein_min = Ein
-                    min_s = s
-                    min_theta = theta
+        Ein_min, ms, mt = find_min_Ein(x,y,split_point)
         Ein_list.append(Ein_min)
     print np.average(Ein_list)
     util.create_histogram(Ein_list)
--- hw2/utilities.py
+++ hw2/utilities.py
@@ -5,6 +5,7 @@ import ConfigParser
 LOWER_BOUND = -1
 UPPER_BOUND = 1
 SPLIT_POINT_PRECISION = 5
+SPLIT_POINT_DEFAULT = 0.0000001
 
 def generate_data(
         data_size, flipping_prob,
@@ -12,7 +13,7 @@ def generate_data(
         split_point_precision=SPLIT_POINT_PRECISION):
     x = np.random.uniform(lower_bound,upper_bound,data_size)
     y = [util.sign(i * util.flip_sign_with_probability(flipping_prob)) for i in x]
-    split_point = (upper_bound - lower_bound)/split_point_precision
+    split_point = float(upper_bound - lower_bound)/data_size/split_point_precision
 
     return x, y, split_point
 
@@ -23,3 +24,22 @@ def get_config():
     data_size = int(config.get('one_dimensional_perceptron', 'data_size'))
     flipping_prob = float(config.get('one_dimensional_perceptron', 'flipping_prob'))
     return loop_time, data_size, flipping_prob
+
+def find_min_Ein(x, y, split_point=SPLIT_POINT_DEFAULT):
+    theta_position_list = [i-split_point for i in x]
+    Ein_min = 1.1
+    min_theta = -1.1
+    min_s = 0
+    err = 0
+    data_size = len(x)
+    for theta in theta_position_list:
+        for s in range (-1,3,2):
+            for n in range(len(x)):
+                if s*util.sign(x[n]-theta) != y[n]:
+                    err += 1
+            Ein = float(err) / (data_size*2)
+            if Ein <= Ein_min:
+                Ein_min = Ein
+                min_s = s
+                min_theta = theta
+    return Ein_min, min_s, min_theta
